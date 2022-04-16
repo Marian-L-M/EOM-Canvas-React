7640f8c4b28c2104c485823514e2b1c484f59bcb
MarianMaschkeRGST <m.maschke@rgst.net>
2022-04-16 22:16:16 +0900

    initial commit with canvas integration template

---
+++ eom_frontend/src/App.js
@@ -0,0 +1,12 @@
+import CanvasElement from './components/CanvasElement'
+
+function App() {
+  return (
+    <div className="App">
+      <CanvasElement />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ eom_frontend/src/components/CanvasElement.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Canvas from './subcomponent/Canvas'
+
+function CanvasElement() {
+  return (
+    <div>
+        <Canvas/>
+    </div>
+  )
+}
+
+export default CanvasElement
